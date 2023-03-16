e6c7a048b5a2e693eb4741a7e6a84eb777ebfcbb
anshulpodaar <anshulpodaar@gmail.com>
2023-03-16 01:14:15 +0100

    20 - Valid Parentheses

--- 020 - Valid Parentheses.py
+++ 020 - Valid Parentheses.py
@@ -20,6 +20,10 @@ Example 3:
 Input: s = "(]"
 Output: false
 
+Example 4:
+Input: s = "([)]"
+Output: false
+
 Constraints:
 1 <= s.length <= 104
 s consists of parentheses only '()[]{}'.
@@ -27,14 +31,21 @@ s consists of parentheses only '()[]{}'.
 
 
 class Solution:
-    parentheses_map = {"(": ")", "{": "}", "[": "]"}
-
     def isValid(self, s: str) -> bool:
-        valid = None
-        # for i, item in enumerate(s.keys()):
-        #     if s(item) in
-        # TODO: create method
-        return valid
+        parentheses_map = {")": "(", "}": "{", "]": "["}
+        check_list = []
+        for item in s:
+            if item in parentheses_map.values():
+                check_list.append(item)
+            elif item in parentheses_map.keys():
+                if len(check_list) > 0 and check_list[-1] == parentheses_map[item]:
+                    check_list.pop(-1)
+                else:
+                    return False
+        if len(check_list) == 0:
+            return True
+        else:
+            return False
 
 
 def _main():
@@ -44,7 +55,8 @@ def _main():
 
     print(Solution.isValid(Solution(), "{()}"))
     print(Solution.isValid(Solution(), "{(})"))
-    print(Solution.isValid(Solution(), "{]"))
+    print(Solution.isValid(Solution(), "){}"))
+    print(Solution.isValid(Solution(), "("))
 
 
 if __name__ == "__main__":
